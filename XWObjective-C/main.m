14be4e6f653f270f28754002837e813d777f440a
viviwu <vivi705@qq.com>
2017-07-24 12:54:26 +0800

    Initial Commit

---
+++ XWObjective-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  XWObjective-C
+//
+//  Created by vivi wu on 2017/7/24.
+//  Copyright © 2017年 vivi wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
